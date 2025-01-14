417d306735ad372741aff1db5f1adccd739a1045
BhardwajNkl <nikhilbhardwaj91099@gmail.com>
2025-01-14 18:18:19 +0530

    winston log files saving to a dedicated folder.

--- src/config/winston-logger.config.ts
+++ src/config/winston-logger.config.ts
@@ -1,7 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import * as winston from 'winston';
 
-export const getWinstonLogger=(configService: ConfigService):winston.Logger =>{
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+export const getWinstonLogger= async (configService: ConfigService):Promise<winston.Logger> =>{
+    const logDirectory = configService.get<string>('WINSTON_LOG_DIR')
+    try{
+        await fs.access(logDirectory);
+    } catch(error){
+        await fs.mkdir(logDirectory);
+    }
+    
     const logger = winston.createLogger({
         level: 'info', // Base log level.
         format: winston.format.combine(
@@ -10,8 +20,8 @@ export const getWinstonLogger=(configService: ConfigService):winston.Logger =>{
         ),
         // defaultMeta: { service: configService.get<string>('APP_NAME') },
         transports: [
-            new winston.transports.File({ filename: 'error.log', level: 'error' }), // Error and higher severity logs go to this file.
-            new winston.transports.File({ filename: 'combined.log' }), // All logs including base level and above go to this file.
+            new winston.transports.File({ filename: path.join(logDirectory, 'error.log') , level: 'error' }), // Error and higher severity logs go to this file.
+            new winston.transports.File({ filename: path.join(logDirectory, 'combined.log') }), // All logs including base level and above go to this file.
         ],
     });
     return logger;
